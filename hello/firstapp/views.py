d60d61c56ee5a96a6180809ea1796d6fa462d07c
45panov <khorkov.anton@gmail.com>
2023-03-13 20:29:59 +0300

    Basics of styles and teplates

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,32 +1,14 @@
+from django.http import *
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from .forms import UserForm
 
 def index(request):
-    #return render(request, "firstapp/home.html")
-    header = "Персональные данные"                      # обычная переменная
-    langs = ["Английский", "Немецкий", "Испанский"]     # массив
-    user = {"name": "Максим", "age": 30}                # словарь
-    addr = ("Виноградная", 23, 45)                      # кортеж
-    data = {"header": header, "langs": langs, "user": user, "address": addr}
-    return render(request, "index.html", context=data)
-
-def about(request):
-    return HttpResponse("<h2>О сайте</h2>")
-
-def contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
-
-def products(request, productid=1):
-    category = request.GET.get("cat", "")
-    output = "<h2>Продукт № {0} Категория: {1}</h2>"\
-        .format(productid, category)
-    return HttpResponse(output)
-
-def users(request, id=1, name="Максим"):
-    id = request.GET.get("id", 1)
-    name = request.GET.get("name", "Максим")
-    output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3>"\
-        .format(id, name)
-    return HttpResponse(output)
+    userform = UserForm()
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            name = userform.cleaned_data["name"]
+            return HttpResponse(f"<h2>Имя введено корректно - "
+                                f"{name}</h2>")
+    return render(request, "firstapp/index.html",
+                  {"form": userform})
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -19,10 +19,4 @@ from firstapp import views
 
 urlpatterns = [
     path('', views.index),
-    re_path(r'^about', views.about),
-    re_path(r'^contact', views.contact),
-    #path('products/', views.products), # маршрут по умолчанию
-    path('products/<int:productid>/', views.products),
-    path('users/', views.users), # маршрут по умолчанию
-    #path('users/<int:id>/<name>/', views.users),
-]
+   ]
